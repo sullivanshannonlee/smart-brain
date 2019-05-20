d92edd9d0c1e9a0a066536d40bc5244d3317373a
Shannon Sullivan <sullivanshannonlee@gmail.com>
2019-05-20 15:44:22 -0700

    changing response

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('it is working!');
 })
 
 app.post('/signin', (req, res) => { signin.handleSignin (req, res, bcrypt, db)})
