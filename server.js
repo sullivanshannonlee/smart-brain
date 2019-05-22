a1a735408db4ccfa25c97d1faebf453725d2a7ba
Shannon Sullivan <sullivanshannonlee@gmail.com>
2019-05-21 20:24:11 -0700

    more stuff

--- server.js
+++ server.js
@@ -25,28 +25,24 @@ const db = knex ({
         }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-});
+// db.select('*').from('users').then(data => {
+//     console.log(data);
+// });
 
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors())
 
 
-app.get('/', (req, res) => {
-    res.send('it is working!');
-})
+app.get('/', (req, res) => {res.send('it is working!')})
 
 app.post('/signin', (req, res) => { signin.handleSignin (req, res, bcrypt, db)})
 
-
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
- 
        
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
